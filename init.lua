312f723ad307540e6f9d4e84184df7d3b5ff94ab
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2025-03-09 20:53:07 +0000

    Auto-format [nix] files.

--- init.lua
+++ init.lua
@@ -932,6 +932,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+
+        nix = { 'nixfmt' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
